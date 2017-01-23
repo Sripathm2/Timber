4edfac4c6609a702272713d74f790ee39b17a499
Sripath Mishra <sripathm22@users.noreply.github.com>
2017-01-23 14:26:45 -0500

    Update ScrobbleQuery.java

--- Timber-master/Timber-master/app/src/main/java/com/naman14/timber/lastfmapi/models/ScrobbleQuery.java
+++ Timber-master/Timber-master/app/src/main/java/com/naman14/timber/lastfmapi/models/ScrobbleQuery.java
@@ -3,9 +3,6 @@ package com.naman14.timber.lastfmapi.models;
 import com.google.gson.annotations.SerializedName;
 import com.naman14.timber.lastfmapi.LastFmClient;
 
-/**
- * Created by christoph on 17.07.16.
- */
 public class ScrobbleQuery {
     private static final String ARTIST_NAME = "artist";
     private static final String TRACK_NAME = "track";
